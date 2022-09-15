f938eb679aa774751d93728bcf91cda389371b97
Jan Lána <lana.jan@gmail.com>
2022-09-15 21:05:16 +0200

    add intellij project files

---
+++ src/cz/gyarab/prg2/s1/Main.java
@@ -0,0 +1,8 @@
+package cz.gyarab.prg2.s1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Ahoj světe");
+    }
+}
---
+++ src/cz/gyarab/prg2/s2/Main.java
@@ -0,0 +1,7 @@
+package cz.gyarab.prg2.s2;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Ahoj druhá skupino!!");
+    }
+}
